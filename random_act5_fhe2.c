99e600e0da8585d96c2453c7d325eb3eefb9d809
FelicityE <fhe2@nau.edu>
2021-02-18 19:37:46 -0700

    Act5 shorten

--- random_act5_fhe2.c
+++ random_act5_fhe2.c
@@ -52,16 +52,16 @@ int main(int argc, char **argv) {
   for(i = 0; i < TOTALITER; i++){
     if(my_rank == current_rank){
       next_rank = generateRandomRank(nprocs-1, my_rank);
-      printf("My rank: %d, next to recv: %d\n\n", my_rank, next_rank);
+      printf("My rank: %d, next to recv: %d\n", my_rank, next_rank);
       MPI_Send(&my_counter, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
     }
     MPI_Bcast(&next_rank, 1, MPI_INT, current_rank, MPI_COMM_WORLD);
     if(my_rank == next_rank){
       MPI_Recv(&myBuffer, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
       my_counter = myBuffer;
-      printf("My rank: %d, old counter: %d\n\n",my_rank, my_counter);
+      printf("My rank: %d, old counter: %d\n",my_rank, my_counter);
       my_counter += my_rank;
-      printf("My rank: %d, new counter: %d\n\n",my_rank, my_counter);
+      printf("My rank: %d, new counter: %d\n",my_rank, my_counter);
       current_rank = my_rank;
     }
     MPI_Bcast(&current_rank, 1, MPI_INT, next_rank, MPI_COMM_WORLD);
